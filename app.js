29586534b4d162ae5cbccf9fd4963e31030cf44c
aishuupp99 <89593681+aishuupp99@users.noreply.github.com>
2021-11-03 13:12:58 -0500

    selector added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var languageRouter = require('./routes/language');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/language', languageRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 
 // catch 404 and forward to error handler
